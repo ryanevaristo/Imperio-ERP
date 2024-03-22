35c7fb917c485fed0b6ed4e83e4cbf5d6bcb74e8
Ryan Evaristo <ryanevaristo007@gmail.com>
2024-03-21 23:16:58 -0300

    restruturando projeto

---
+++ financeiro/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+from .models import ContaPagar, ContaReceber, Cheque
+@admin.register(ContaPagar)
+class ContaPagarAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'valor', 'data_vencimento', 'forma_pagamento', 'pago')
+    list_filter = ('pago', 'forma_pagamento')
+    search_fields = ('descricao', 'forma_pagamento')
+    date_hierarchy = 'data_vencimento'
+
+@admin.register(ContaReceber)
+class ContaReceberAdmin(admin.ModelAdmin):
+    list_display = ('descricao', 'valor', 'data_vencimento', 'forma_recebimento', 'recebido')
+    list_filter = ('recebido', 'forma_recebimento')
+    search_fields = ('descricao', 'forma_recebimento')
+    date_hierarchy = 'data_vencimento'
+
+@admin.register(Cheque)
+class ChequeAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'nome_titular', 'situacao', 'data_compensacao', 'valor')
+    list_filter = ('situacao', 'nome_titular')
+    search_fields = ('numero', 'nome_titular')
+
+
+
---
+++ financeiro/models.py
@@ -0,0 +1,103 @@
+from django.db import models
+from datetime import datetime
+
+
+class ContaPagar(models.Model):
+
+    choice_forma_pagamento = (
+        ('D', 'Dinheiro'),
+        ('C', 'Cartão de Crédito'),
+        ('B', 'Boleto'),
+        ('T', 'Transferência Bancária'),
+        ('C', 'Cheque'),
+        ('P', 'PIX')
+    )
+
+    descricao = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data_vencimento = models.DateField(null=True, blank=True, default=datetime(1900, 1, 1))
+    data_pagamento = models.DateField(null=True, blank=True, default=datetime(1900, 1, 1))
+    forma_pagamento = models.CharField(max_length=1, choices=choice_forma_pagamento,default='D',null=True, blank=True)
+    pago = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.descricao
+    
+    def get_valor(self):
+        return "R$ " + str(self.valor).replace('.', ',')
+    
+    def get_data_vencimento(self):
+        return self.data_vencimento.strftime('%d/%m/%Y')
+    
+
+class ContaReceber(models.Model):
+    choice_forma_recebimento = (
+        ('D', 'Dinheiro'),
+        ('C', 'Cartão de Crédito'),
+        ('B', 'Boleto'),
+        ('T', 'Transferência Bancária'),
+        ('C', 'Cheque')
+    )
+    descricao = models.CharField(max_length=100)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    data_vencimento = models.DateField()
+    data_recebimento = models.DateField(null=True, blank=True)
+    recebido = models.BooleanField(default=False)
+    forma_recebimento = models.CharField(max_length=1, choices=choice_forma_recebimento,default='D',null=True, blank=True)
+
+    def __str__(self):
+        return self.descricao
+    
+    def get_valor(self):
+        return "R$ " + str(self.valor).replace('.', ',')
+    
+    def get_data_vencimento(self):
+        return self.data_vencimento.strftime('%d/%m/%Y')
+    
+class Cheque(models.Model):
+    choice_situacao = (
+        ('E', 'Emitido'),
+        ('C', 'Compensado'),
+        ('V', 'Vencido'),
+        ('S', 'Sem Fundo'),
+        ('D', 'Devolvido')
+    )
+    choice_banco = (
+        ('001', 'Banco do Brasil'),
+        ('104', 'Caixa Econômica Federal'),
+        ('237', 'Bradesco'),
+        ('341', 'Itaú'),
+        ('356', 'Santander'),
+        ('033', 'Banco Santander (Brasil)'),
+        ('745', 'Citibank'),
+        ('399', 'HSBC'),
+        ('422', 'Safra'),
+        ('389', 'Mercantil do Brasil'),
+        ('633', 'Rendimento'),
+        ('652', 'Itaú Unibanco Holding'),
+        ('745', 'Banco Citibank'),
+        ('748', 'Sicredi'),
+        ('756', 'Sicoob')
+    )
+    numero = models.CharField(max_length=20)
+    nome_titular = models.CharField(max_length=100)
+    nome_repassador = models.CharField(max_length=100, null=True, blank=True)
+    valor = models.DecimalField(max_digits=10, decimal_places=2)
+    banco = models.CharField(max_length=3, choices=choice_banco, default='001',null=True, blank=True)
+    data_compensacao = models.DateField(null=True, blank=True, default=datetime(1900, 1, 1))
+    situacao = models.CharField(max_length=1, choices=choice_situacao,default='E',null=True, blank=True)
+
+    def __str__(self):
+        return self.numero
+    
+    def get_valor(self):
+        return "R$ " + str(self.valor).replace('.', ',')
+    
+    def get_data_emissao(self):
+        return self.data_emissao.strftime('%d/%m/%Y')
+    
+    def get_data_compensacao(self):
+        return self.data_compensacao.strftime('%d/%m/%Y')
+    
+    
+# Create your models here.
