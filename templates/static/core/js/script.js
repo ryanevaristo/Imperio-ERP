da5950e8ec4184a9e7edde1416068c3747a4461d
ryanevaristo <ryanevaristo007@gmail.com>
2024-05-20 22:26:43 -0300

    add regra sidebar mobile

--- templates/static/core/js/script.js
+++ templates/static/core/js/script.js
@@ -1,6 +1,25 @@
+function adjustSidebar() {
+  var sidebar = document.getElementById('sidebar');
+  var overlay = document.querySelector(".overlay");
+  
+  if (window.innerWidth > 700) {
+    sidebar.classList.add("expand");
+  } else {
+    sidebar.classList.remove("expand");
+    overlay.style.display = "block";
+  }
+}
+
+// Call the function immediately on page load
+adjustSidebar();
+
+// Also call the function on window resize
+window.addEventListener('resize', adjustSidebar);
+
 const hamBurger = document.querySelector(".toggle-btn");
 
 hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
+
 
