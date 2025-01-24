ed53e51e37a0b2a7f6a2cba36af36a8b6e21e03b
Ryan Evaristo <ryanevaristo007@gmail.com>
2025-01-24 17:15:27 -0300

    add home estoque

--- estoque/urls.py
+++ estoque/urls.py
@@ -4,4 +4,10 @@ from . import views
 app_name = 'estoque'
 
 urlpatterns = [
+    path('', views.home_estoque, name='home_estoque'),
+    path('cadastrar_produto/', views.cadastrar_produto, name='cadastrar_produto'),
+    path('editar_produto/<uuid:id>/', views.editar_produto, name='editar_produto'),
+    path('deletar_produto/<uuid:id>/', views.deletar_produto, name='deletar_produto'),
+    path('detalhes_produto/<uuid:id>/', views.detalhes_produto, name='detalhes_produto'),
+
 ]
--- imperio/settings.py
+++ imperio/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'financeiro',
     'cliente',
     'produto',
+    'estoque',
 ]
 
 MIDDLEWARE = [
